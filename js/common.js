ca108a5917c02a35399d371def8e33fb6b3ff64b
Roman <rromaromik@gmail.com>
2017-06-17 16:32:04 +0300

    rebuild project

--- js/common.js
+++ js/common.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+	//slides
 		$('.owl-team').owlCarousel({
 		items: 4,
 		loop: true,
@@ -65,6 +67,7 @@ $(document).ready(function () {
 		smartSpeed: 700
 		});
 
+
 		$('.filter-right__item_txt').click(function(){
 			if (!$(this).hasClass('active')) {
 	    		$('.filter-right__item_txt').removeClass('active');
@@ -72,6 +75,7 @@ $(document).ready(function () {
 	    	}
 		});
 
+		//filter
 		$('.menu__item').click(function(){
 			if (!$(this).hasClass('active-h')) {
 	    		$('.menu__item').removeClass('active-h');
@@ -79,7 +83,7 @@ $(document).ready(function () {
 	    	}
 		});
 
-
+		//animation content
 		(function($) {
 			$.fn.animated = function(inEffect) {
 				$(this).each(function() {
@@ -96,22 +100,34 @@ $(document).ready(function () {
 			};
 		})(jQuery);
 
-		$(".about__mac").animated("fadeInLeft");
-		$(".about__text").animated("fadeInRight");
-		$(".owl-item").animated("fadeInUp");
-		$(".services").animated("lightSpeedIn");
-		$(".content-portfolio").animated("fadeInLeft");
-		$(".wr-contact__txt").animated("fadeInRight");
 
+		//deleted animation for mobile
+		var isMobile = {
+		    Android:        function() { return navigator.userAgent.match(/Android/i) ? true : false; },
+		    BlackBerry:     function() { return navigator.userAgent.match(/BlackBerry/i) ? true : false; },
+		    iOS:            function() { return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false; },
+		    Windows:        function() { return navigator.userAgent.match(/IEMobile/i) ? true : false; },
+		    any:            function() { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());  }
+		};
+  
+		if ( !isMobile.any() ) {
+    		$(".about__mac").animated("fadeInLeft");
+				$(".about__text").animated("fadeInRight");
+				$(".owl-item").animated("fadeInUp");
+				$(".services").animated("lightSpeedIn");
+				$(".content-portfolio").animated("fadeInLeft");
+				$(".wr-contact__txt").animated("fadeInRight");
+		}
 
-		
-
+		//burger animation
 		$('.burger').click(function(){
 			$('.burger__line').toggleClass('active-b');
 			$('.b-menu').toggleClass('open');
 
 		});
 
+
+		//fixed header
 
 		// $(document).on('scroll', function(){
 		// 	var header = $('.header');
@@ -124,9 +140,65 @@ $(document).ready(function () {
 		// 	}
 		// });
 
-
+		//preloader
 		$('.preloader').fadeOut();
 
+
+
+		$("a.arrowDown__font").click(function() {
+		    var elementClick = $(this).attr("href");
+		    var destination = $(elementClick).offset().top;
+		    jQuery("html:not(:animated),body:not(:animated)").animate({
+		      scrollTop: destination
+		    }, 1200);
+		    return false;
+  		});
+
+
+
+
+  		//jquery-validation
+
+
+			$('#form-message').validate({
+			rules:{
+
+		            form__name:{
+			                required: true,
+			                minlength: 4,
+			                maxlength: 16,
+			            },
+
+			            form__email:{
+			                required: true,
+			                email: true,
+			            },
+
+			            form__message:{
+			            	required: true,
+			            	minlength: 10,
+			            },
+	       			},
+
+		         messages:{
+
+		            form__name:{
+		            	minlength: "Имя должно состоять минимум из 4-х символов",
+		                required: "Это поле обязательно для заполнения",
+		            },
+
+		            form__email:{
+		                required: "Это поле обязательно для заполнения",
+		                email: "Ведите правильный Email адресс",
+		            },
+
+		            form__message:{
+		                required: "Это поле обязательно для заполнения",
+		                minlength: "Минимум 10 символов"
+		            },
+
+		       },
+			});
 
 });
 
